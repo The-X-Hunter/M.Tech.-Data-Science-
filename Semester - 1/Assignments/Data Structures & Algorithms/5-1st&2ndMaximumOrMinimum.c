85cae1c89aacd80f064d169dad1dd70a9e437d33
newbie_tester <newbie@localhost.localdomain>
2023-01-04 16:20:45 +0530

    Updated 5-1st&2ndMaximumOrMinimum.c, replaced heap based solution with selection sort so the time complexity decresed from O(nlogn)->O(n).

--- Semester - 1/Assignments/Data Structures & Algorithms/5-1st&2ndMaximumOrMinimum.c
+++ Semester - 1/Assignments/Data Structures & Algorithms/5-1st&2ndMaximumOrMinimum.c
@@ -11,6 +11,8 @@
 
 int sequence[] = {2, 5, 10, 2, 4};
 
+/*
+Part of heap based solution
 int printHeap(int heap[], int numberOfElements){
     int i, j, power, index = 0;
     for(i = 0; (i < numberOfElements) && (index < numberOfElements); i++){
@@ -22,8 +24,22 @@ int printHeap(int heap[], int numberOfElements){
         printf("\n");
     }
 }
+*/
 
-int findMinimum(int sequence[], int numberOfElements){
+int print(int numberOfElements){
+    int i;
+    printf("Sequence is:\n");
+    for(i = 0; i < numberOfElements; i++){
+        printf("%d\t", sequence[i]);
+    }
+    printf("\n");
+    return 0;
+}
+
+/*
+These modules are using heap structure and which increases coplexity to O(nlogn)
+
+int findMinimum1(int sequence[], int numberOfElements){
     int i, j, parentIndex;
     int minHeap[numberOfElements];
     for(i = 0; i < numberOfElements; i++){
@@ -49,7 +65,7 @@ int findMinimum(int sequence[], int numberOfElements){
     return 0;
 }
 
-int findMaximum(int sequence[], int numberOfElements){
+int findMaximum1(int sequence[], int numberOfElements){
     int i, j, parentIndex;
     int maxHeap[numberOfElements];
     for(i = 0; i < numberOfElements; i++){
@@ -71,6 +87,68 @@ int findMaximum(int sequence[], int numberOfElements){
     printf("Max heap:\n");
     printHeap(maxHeap, numberOfElements);
     printf("So the 1st & 2nd maximums are %d & %d, respectively.\n", maxHeap[0], maxHeap[1]);
+    printf("******************************************\n");
+    return 0;
+}
+*/
+
+//Built using selection sort with outer loop restricted for only 2 elements
+//So the time complexity is 2 * n ~ O(n)
+
+int findMinimum(int sequence[], int numberOfElements){
+    int i, j, minValue, minValueIndex, firstMinValue, firstMinValueIndex;
+    for(i = 0; i < 2; i++){
+        minValue = sequence[0];
+        minValueIndex = 0;
+        for(j = 1; j < numberOfElements; j++){
+            if(i > 0){
+                if((sequence[j] < minValue && sequence[j] >= firstMinValue) && (j != firstMinValueIndex)){
+                    minValue = sequence[j];
+                    minValueIndex = j;
+                }
+            } else {
+                if(sequence[j] < minValue){
+                    minValue = sequence[j];
+                    minValueIndex = j;
+                }
+            }
+        }
+        if(i == 0) {
+            firstMinValue = minValue;
+            firstMinValueIndex = minValueIndex;
+        }
+    }
+    print(numberOfElements);
+    printf("So the 1st & 2nd minimums are %d & %d, respectively.\n", firstMinValue, minValue);
+    printf("******************************************\n");
+    return 0;
+}
+
+int findMaximum(int sequence[], int numberOfElements){
+    int i, j, maxValue, maxValueIndex, firstMaxValue, firstMaxValueIndex;
+    for(i = 0; i < 2; i++){
+        maxValue = sequence[0];
+        maxValueIndex = 0;
+        for(j = 1; j < numberOfElements; j++){
+            if(i > 0){
+                if((sequence[j] > maxValue && sequence[j] <= firstMaxValue) && (j != firstMaxValueIndex)){
+                    maxValue = sequence[j];
+                    maxValueIndex = j;
+                }
+            } else {
+                if(sequence[j] > maxValue){
+                    maxValue = sequence[j];
+                    maxValueIndex = j;
+                }
+            }
+        }
+        if(i == 0){
+            firstMaxValue = maxValue;
+            firstMaxValueIndex = maxValueIndex;
+        }
+    }
+    print(numberOfElements);
+    printf("So the 1st & 2nd maximums are %d & %d, respectively.\n", firstMaxValue, maxValue);
     printf("******************************************\n");
     return 0;
 }
